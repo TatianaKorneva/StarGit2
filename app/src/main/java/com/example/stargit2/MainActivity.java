10d0d51e9346282549bd9c577622e5d2a8fa16b3
TatianaKorneva <irreal13@gmail.com>
2019-03-17 12:20:55 +0300

    Удаление лишнего

--- app/src/main/java/com/example/stargit2/MainActivity.java
+++ app/src/main/java/com/example/stargit2/MainActivity.java
@@ -6,13 +6,11 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.button);
     }
 }
