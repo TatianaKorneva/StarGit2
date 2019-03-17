77fc92772f30fb6a95fa174d3ee742b0fbe717ac
TatianaKorneva <irreal13@gmail.com>
2019-03-17 12:52:01 +0300

    Добавление всего необходимого

--- app/src/main/java/com/example/stargit2/MainActivity.java
+++ app/src/main/java/com/example/stargit2/MainActivity.java
@@ -2,15 +2,28 @@ package com.example.stargit2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    int Count = 0;
+    TextView mainTextView;
+    Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainTextView = findViewById(R.id.main_textview);
+        mainButton = findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        mainTextView.setText("Ты нажал на кнопку целых " + ++Count + " раз(а)!!!");
     }
 }
+
+
